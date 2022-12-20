85abbf834657070742ce3dfa43701b48c8ec2ea3
jonathan <jonathanbajoux.pro@gmail.com>
2022-12-20 11:15:49 +0100

    jonathan

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('press1');
+
+let addDiv = document.createElement('div');
+document.body.appendChild(addDiv);
+
+//button.addEventListener("click", ()=> fetchFile());
+
